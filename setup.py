006edefc12ffe1debdd4a9933634a0be425f505c
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-04-02 18:50:26 +0500

    Autoenv paketi yaratildi: .env fayllarini boshqarish uchun

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="autoenv",  # Paket nomi
-    version="0.1.0",  # Paket versiyasi
-    packages=find_packages(),  # Paketni aniqlash
-    install_requires=[  # Talablar (agar boshqa paketlarga bog'liq bo'lsa)
-        'environs>=9.5.0',  # .env fayllari bilan ishlash uchun environs kutubxonasi
-        'pytest>=7.0.0',     # Testlar uchun pytest kutubxonasi
+    name="autoenv",                 # Paket nomi
+    version="0.1",                  # Paket versiyasi
+    packages=find_packages(),       # Paketni aniqlash
+    install_requires=[              # Talablar (agar boshqa paketlarga bog'liq bo'lsa)
+        'requests',                 # Misol uchun, agar `requests` paketi kerak bo'lsa
+        'environs',                 # Ensuring `environs` is included for installation
     ],
-    entry_points={  # Komanda liniyasida ishlash
+    entry_points={                  # Komanda liniyasida ishlash
         'console_scripts': [
             'autoenv=autoenv.cli:autoenv',  # autoenv komandasi `autoenv.cli:autoenv` ga ulanishi kerak
         ],
@@ -16,11 +16,12 @@ setup(
     long_description=open('README.md').read(),  # README faylini qo'shish
     long_description_content_type='text/markdown',  # README fayli markdown formatida
     url='https://github.com/Husanjonazamov/autoenv',  # GitHub URL
-    author='Husanjon Azamov',  # Muallif nomi
+    author='Husanjon Azamov',                    # Muallif nomi
     author_email='azamovhusanboy@gmail.com',  # Email manzili
-    classifiers=[  # PyPI uchun tasniflar
+    classifiers=[                          # PyPI uchun tasniflar
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
+    # license_file='LICENSE',  # Bu qatordagi 'license-file'ni olib tashlash
 )
