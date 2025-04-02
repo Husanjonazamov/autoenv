985b61013285e06078703fd8e7929afd5eac6996
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-04-02 18:52:15 +0500

    setup.py: O'zgartirishlar kiritildi, boshqa fayllar yangilandi

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="autoenv",                 # Paket nomi
-    version="0.1",                  # Paket versiyasi
-    packages=find_packages(),       # Paketni aniqlash
-    install_requires=[              # Talablar (agar boshqa paketlarga bog'liq bo'lsa)
-        'requests',                 # Misol uchun, agar `requests` paketi kerak bo'lsa
-        'environs',                 # Ensuring `environs` is included for installation
+    name="autoenv",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'environs',
     ],
-    entry_points={                  # Komanda liniyasida ishlash
+    entry_points={
         'console_scripts': [
-            'autoenv=autoenv.cli:autoenv',  # autoenv komandasi `autoenv.cli:autoenv` ga ulanishi kerak
+            'autoenv=autoenv.cli:autoenv',
         ],
     },
-    long_description=open('README.md').read(),  # README faylini qo'shish
-    long_description_content_type='text/markdown',  # README fayli markdown formatida
-    url='https://github.com/Husanjonazamov/autoenv',  # GitHub URL
-    author='Husanjon Azamov',                    # Muallif nomi
-    author_email='azamovhusanboy@gmail.com',  # Email manzili
-    classifiers=[                          # PyPI uchun tasniflar
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Husanjonazamov/autoenv',
+    author='Husanjon Azamov',
+    author_email='azamovhusanboy@gmail.com',
+    classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    # license_file='LICENSE',  # Bu qatordagi 'license-file'ni olib tashlash
 )
